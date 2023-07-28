4e51bd6f023f232118dc435973d30b6580cb53af
mellhub <mariaeduardalopeslima2@hotmail.com>
2023-07-28 15:22:45 -0400

    Create parent-data.component.ts

---
+++ Youtube/CursoAngular13Introducao-01-2022/curso-angular-y/src/app/components/parent-data/parent-data.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parent-data',
+  templateUrl: './parent-data.component.html',
+  styleUrls: ['./parent-data.component.css']
+})
+export class ParentDataComponent {
+
+}
