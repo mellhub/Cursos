7d1587d1d47fc20e20a892390591abc0330211e4
mellhub <mariaeduardalopeslima2@hotmail.com>
2023-08-04 14:57:12 -0400

    Update app.module.ts

--- Youtube/CursoAngular13Introducao-01-2022/curso-angular-y/src/app/app.module.ts
+++ Youtube/CursoAngular13Introducao-01-2022/curso-angular-y/src/app/app.module.ts
@@ -5,12 +5,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
 import { ParentDataComponent } from './components/parent-data/parent-data.component';
+import { DirectivesComponent } from './components/directives/directives.component';
+import { IfRenderComponent } from './components/if-render/if-render.component';
+import { EventosComponent } from './components/eventos/eventos.component';
+import { EmitterComponent } from './components/emitter/emitter.component';
+import { ChangeNumberComponent } from './components/change-number/change-number.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponentComponent,
-    ParentDataComponent
+    ParentDataComponent,
+    DirectivesComponent,
+    IfRenderComponent,
+    EventosComponent,
+    EmitterComponent,
+    ChangeNumberComponent
   ],
   imports: [
     BrowserModule,
