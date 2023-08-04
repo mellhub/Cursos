301876a2feb84a9b08b8fabebab4acfe925e34c4
mellhub <mariaeduardalopeslima2@hotmail.com>
2023-08-04 14:57:06 -0400

    Update app.component.ts

--- Youtube/CursoAngular13Introducao-01-2022/curso-angular-y/src/app/app.component.ts
+++ Youtube/CursoAngular13Introducao-01-2022/curso-angular-y/src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  userName = 'Julia';
+
+  userData = {
+    email: 'julia@email.com',
+    role: 'Admin',
+  }
+
   title = 'curso-angular-y';
 }
