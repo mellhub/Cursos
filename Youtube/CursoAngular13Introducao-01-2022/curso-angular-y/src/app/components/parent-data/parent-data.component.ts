122c24e07fc4b1648f288ba9752ef9cdf0a5174d
mellhub <mariaeduardalopeslima2@hotmail.com>
2023-08-04 14:58:44 -0400

    Update parent-data.component.ts

--- Youtube/CursoAngular13Introducao-01-2022/curso-angular-y/src/app/components/parent-data/parent-data.component.ts
+++ Youtube/CursoAngular13Introducao-01-2022/curso-angular-y/src/app/components/parent-data/parent-data.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-parent-data',
   templateUrl: './parent-data.component.html',
   styleUrls: ['./parent-data.component.css']
 })
-export class ParentDataComponent {
+export class ParentDataComponent implements OnInit {
+  @Input() name: string = '';
+  @Input() userData!: {email: string, role: string};
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
