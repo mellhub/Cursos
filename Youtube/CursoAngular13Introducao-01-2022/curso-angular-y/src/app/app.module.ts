a1a2eaca8be42a84c106a542c1f8161f577ba117
mellhub <mariaeduardalopeslima2@hotmail.com>
2023-08-07 14:41:13 -0400

    Update app.module.ts

--- Youtube/CursoAngular13Introducao-01-2022/curso-angular-y/src/app/app.module.ts
+++ Youtube/CursoAngular13Introducao-01-2022/curso-angular-y/src/app/app.module.ts
@@ -10,6 +10,8 @@ import { IfRenderComponent } from './components/if-render/if-render.component';
 import { EventosComponent } from './components/eventos/eventos.component';
 import { EmitterComponent } from './components/emitter/emitter.component';
 import { ChangeNumberComponent } from './components/change-number/change-number.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
+import { PipesComponent } from './components/pipes/pipes.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,9 @@ import { ChangeNumberComponent } from './components/change-number/change-number.
     IfRenderComponent,
     EventosComponent,
     EmitterComponent,
-    ChangeNumberComponent
+    ChangeNumberComponent,
+    ListRenderComponent,
+    PipesComponent,
   ],
   imports: [
     BrowserModule,
