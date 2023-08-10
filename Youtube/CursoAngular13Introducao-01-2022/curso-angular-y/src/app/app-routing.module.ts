69d27fac8ff3066b57fc6fb319d095e62654e44a
mellhub <mariaeduardalopeslima2@hotmail.com>
2023-08-10 17:22:00 -0400

    Update app-routing.module.ts

--- Youtube/CursoAngular13Introducao-01-2022/curso-angular-y/src/app/app-routing.module.ts
+++ Youtube/CursoAngular13Introducao-01-2022/curso-angular-y/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { ListRenderComponent } from './components/list-render/list-render.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ItemDetailComponent } from './components/item-detail/item-detail.component';
 
 const routes: Routes = [
   {path: '', component: FirstComponentComponent},
-  {path: 'list', component: ListRenderComponent}
+  {path: 'list', component: ListRenderComponent},
+  {path: 'list/:id', component: ItemDetailComponent },
 ];
 
 @NgModule({
