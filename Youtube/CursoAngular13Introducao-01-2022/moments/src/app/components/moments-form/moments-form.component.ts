64b50bfea4b70869f1addbbafe4ddbe6bd27a834
mellhub <mariaeduardalopeslima2@hotmail.com>
2023-08-24 11:09:50 -0400

    Create moments-form.component.ts

---
+++ Youtube/CursoAngular13Introducao-01-2022/moments/src/app/components/moments-form/moments-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-moments-form',
+  templateUrl: './moments-form.component.html',
+  styleUrls: ['./moments-form.component.css']
+})
+export class MomentsFormComponent {
+
+}
