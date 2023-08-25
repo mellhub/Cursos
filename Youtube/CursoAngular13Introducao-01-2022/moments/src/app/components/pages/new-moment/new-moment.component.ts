e46f3816e566000ee6afae7c9c7a6eba23853868
mellhub <mariaeduardalopeslima2@hotmail.com>
2023-08-25 17:01:40 -0400

    Update new-moment.component.ts

--- Youtube/CursoAngular13Introducao-01-2022/moments/src/app/components/pages/new-moment/new-moment.component.ts
+++ Youtube/CursoAngular13Introducao-01-2022/moments/src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-new-moment',
   templateUrl: './new-moment.component.html',
   styleUrls: ['./new-moment.component.css']
 })
-export class NewMomentComponent {
+export class NewMomentComponent implements OnInit {
+  btnText = 'Compartilhar!';
 
+  constructor() { }
+
+  ngOnInit(): void {}
 }
