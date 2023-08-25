98a344a058c65de33959b07324f01454d18a49c3
mellhub <mariaeduardalopeslima2@hotmail.com>
2023-08-25 17:01:25 -0400

    Update moments-form.component.ts

--- Youtube/CursoAngular13Introducao-01-2022/moments/src/app/components/moments-form/moments-form.component.ts
+++ Youtube/CursoAngular13Introducao-01-2022/moments/src/app/components/moments-form/moments-form.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-moments-form',
   templateUrl: './moments-form.component.html',
   styleUrls: ['./moments-form.component.css']
 })
-export class MomentsFormComponent {
+export class MomentsFormComponent implements OnInit {
+  @Input() btnText!: string
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
