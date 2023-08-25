e405d62f5229006a98182f538c3cfc08b431a334
mellhub <mariaeduardalopeslima2@hotmail.com>
2023-08-25 17:01:12 -0400

    Update app.module.ts

--- Youtube/CursoAngular13Introducao-01-2022/moments/src/app/app.module.ts
+++ Youtube/CursoAngular13Introducao-01-2022/moments/src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { AboutComponent } from './components/pages/about/about.component';
@@ -22,7 +25,10 @@ import { MomentsFormComponent } from './components/moments-form/moments-form.com
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule, 
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
