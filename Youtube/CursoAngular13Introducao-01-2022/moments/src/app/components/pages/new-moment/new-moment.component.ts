980a6e3b6387ae5838448346d6e809f46bdcf3a3
mellhub <mariaeduardalopeslima2@hotmail.com>
2023-08-31 14:37:32 -0400

    Update new-moment.component.ts

--- Youtube/CursoAngular13Introducao-01-2022/moments/src/app/components/pages/new-moment/new-moment.component.ts
+++ Youtube/CursoAngular13Introducao-01-2022/moments/src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Moment } from 'src/app/Moment';
 @Component({
   selector: 'app-new-moment',
   templateUrl: './new-moment.component.html',
@@ -11,4 +11,16 @@ export class NewMomentComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {}
+
+  async createHandler(moment: Moment) {
+    const formData = new FormData()
+
+    formData.append('title', moment.title)
+    formData.append('description', moment.description)
+
+    if(moment.image) {
+      formData.append('image', moment.image)
+    }
+  }
 }
+
