73c82a3f38443147d96587cac993e562e0f28245
mellhub <mariaeduardalopeslima2@hotmail.com>
2023-09-11 15:35:34 -0400

    removo

--- Youtube/CursoAngular13Introducao-01-2022/moments-2/src/app/Comment.ts
+++
@@ -1,8 +0,0 @@
-export interface Comment {
-    id?: number;
-    text: string;
-    username: string;
-    momentId: number;
-    created_at?: string;
-    updated_at?: string;
-}
--- Youtube/CursoAngular13Introducao-01-2022/moments-2/src/app/Moment.ts
+++
@@ -1,9 +0,0 @@
-export interface Moment {
-    id?: number;
-    title: string;
-    description: string;
-    image: string;
-    created_at?: string;
-    updated_at?: string;
-    comments?: [{ text: string; username: string }];
-}
