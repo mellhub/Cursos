16d0371b654c398cd1e7a421ed8b00e6c4148cd9
mellhub <mariaeduardalopeslima2@hotmail.com>
2023-09-11 15:23:49 -0400

    curso angular

--- Youtube/CursoAngular13Introducao-01-2022/moments-2/src/app/components/messages/messages.component.spec.ts
+++ Youtube/CursoAngular13Introducao-01-2022/moments-2/src/app/components/messages/messages.component.spec.ts
@@ -1,16 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NewMomentComponent } from './new-moment.component';
+import { MessagesComponent } from './messages.component';
 
-describe('NewMomentComponent', () => {
-  let component: NewMomentComponent;
-  let fixture: ComponentFixture<NewMomentComponent>;
+describe('MessagesComponent', () => {
+  let component: MessagesComponent;
+  let fixture: ComponentFixture<MessagesComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [NewMomentComponent]
+      declarations: [MessagesComponent]
     });
-    fixture = TestBed.createComponent(NewMomentComponent);
+    fixture = TestBed.createComponent(MessagesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ Youtube/CursoAngular13Introducao-01-2022/moments-2/src/app/components/messages/messages.component.ts
@@ -0,0 +1,19 @@
+import { MessagensService } from './../../services/messagens.service';
+import { Component, OnInit } from '@angular/core';
+
+import { MessagesService } from 'src/app/services/messages/messages.service';
+
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-messages',
+  templateUrl: './messages.component.html',
+  styleUrls: ['./messages.component.css'],
+})
+export class MessagesComponent implements OnInit {
+  faTimes = faTimes;
+
+  constructor(public messagensService: MessagensService) {}
+
+  ngOnInit(): void {}
+}
